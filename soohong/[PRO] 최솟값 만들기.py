088ea2c0cbda9ffbf7a44ae6d2e11612cb72b3d2
2riing <5032303000@naver.com>
2022-10-13 21:31:37 +0900

    [PRO] 최솟값 만들기 15min

--- soohong/[PRO] 최솟값 만들기.py
+++ soohong/[PRO] 최솟값 만들기.py
@@ -1,19 +1,11 @@
-from itertools import permutations
 
 def solution(A, B):
-    answer = 1000 * 1000
+    answer = 0
     N = len(A)
-    permusA = list(permutations(A, N))
-    permusB = list(permutations(B, N))
-    print(permusA)
-    print(permusB)
-    for i in range(len(permusA)):
-        for j in range(len(permusA)):# permutaions 길이
-            temp = 0
-            for k1 in range(N):
-                temp += (permusA[i][k1] * permusB[j][k1])
-        if answer > temp:
-            answer = temp
-    return answer
+    A.sort()
+    B.sort(reverse=True)
 
-solution([1, 2], [3, 4])
+    for i in range(N):
+        answer += A[i] * B[i]
+
+    return answer
